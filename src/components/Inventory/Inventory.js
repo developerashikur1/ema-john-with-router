1614e6b1096702c7b21bd85cb83fd907472e3e2b
ashikur rahman <masik4506@gmail.com>
2021-10-01 01:35:31 +0600

    news

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is Inventory</h2>
+            <h2>This is Inventory now</h2>
         </div>
     );
 };
